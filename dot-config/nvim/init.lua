ea065a53a0ee06152c940c39abc16afdaecb3c34
Lancy <297596661+lancy69@users.noreply.github.com>
2026-07-20 15:38:18 +0800

    feat(nvim): rewrite config and add oil.nvim plugin

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,27 +1,68 @@
+-- Basics {{{
+
+-- Experimental. Load modules faster.
+vim.loader.enable()
+
+-- Use different leader keys.
+-- NOTE: Set before loading plugins for them to use correct leader keys.
 vim.g.mapleader = " "
 vim.g.maplocalleader = ";"
 
-vim.opt.breakindent = true
-vim.opt.confirm = true
-vim.opt.cursorline = true
-vim.opt.ignorecase = true
+-- Yes, I use NeoVim with a mouse!
+-- I betrayed the sacred Vim cult, and should be banished to the VSCode hell.
 vim.opt.mouse = "a"
+
+-- Enable relative line numbers for better navigation.
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 5
+
+-- Keep a margin when scrolling.
+vim.opt.scrolloff = 10
+
+-- Tell me where I am by drawing a cursorline.
+vim.opt.cursorline = true
+
+-- Use a sane value for indentations.
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
-vim.opt.showmode = false
+
+-- Visually indent wrapped lines.
+vim.opt.breakindent = true
+
+-- Enable undo/redo changes even after closing and reopening a file.
+vim.opt.undofile = true
+
+-- Make searches case-insensitive UNLESS patterns contain capital letters.
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Split new panes to down/right instead of up/left.
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.tabstop = 4
-vim.opt.winborder = "double"
 
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- Always draw signcolumn to prevent inconsistent padding for sign/nosign.
+vim.opt.signcolumn = "yes"
 
-vim.pack.add({{
-  name = "tokyonight.nvim",
-  src = "https://github.com/folke/tokyonight.nvim",
-}})
+-- Ask the user for actions instead of throwing an error.
+vim.opt.confirm = true
+
+-- Draw borders to indicate floating windows.
+vim.opt.winborder = "rounded"
+
+-- Fold by markers. Split large file into smaller sections (like this one).
+vim.opt.foldmethod = "marker"
+
+-- }}}
+
+-- Plugins {{{
+
+-- Colorscheme
+vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
 vim.cmd.colorscheme("tokyonight-storm")
+
+-- File Explorer
+vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
+require("oil").setup()
+vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory." })
+
+-- }}}
