939c12d60af0669c2550cc19aae61a521c7f9848
Lancy <297596661+lancy69@users.noreply.github.com>
2026-09-04 14:04:28 +0800

    feat(zsh): update codex env vars

--- dot-config/zsh/plugins/codex.zsh
+++ dot-config/zsh/plugins/codex.zsh
@@ -4,3 +4,7 @@ if (( ! $+commands[codex] )) && [[ ! -d "/Applications/ChatGPT.app" ]]; then
 fi
 
 export CODEX_HOME="$XDG_DATA_HOME/codex"
+# Explicitly set $CODEX_HOME for desktop app to prevent cluttering $HOME.
+if [[ "$(launchctl getenv "CODEX_HOME" 2> /dev/null)" != "$CODEX_HOME" ]]; then
+  launchctl setenv "CODEX_HOME" "$CODEX_HOME"
+fi
